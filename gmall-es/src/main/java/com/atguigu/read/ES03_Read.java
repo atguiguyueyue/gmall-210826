3df474091828c1f291ccdf172f206c76a6e6b557
0826 <0826@atguigu.com>
2022-01-05 10:56:31 +0800

    day05-上午

---
+++ gmall-es/src/main/java/com/atguigu/read/ES03_Read.java
@@ -0,0 +1,102 @@
+package com.atguigu.read;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import io.searchbox.core.search.aggregation.MaxAggregation;
+import io.searchbox.core.search.aggregation.MetricAggregation;
+import io.searchbox.core.search.aggregation.TermsAggregation;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class ES03_Read {
+    public static void main(String[] args) throws IOException {
+        //1.创建客户端工厂
+        JestClientFactory jestClientFactory = new JestClientFactory();
+
+        //2.设置连接属性
+        HttpClientConfig httpClientConfig = new HttpClientConfig.Builder("http://hadoop102:9200").build();
+        jestClientFactory.setHttpClientConfig(httpClientConfig);
+
+        //3.获取连接
+        JestClient jestClient = jestClientFactory.getObject();
+
+        //4.查询ES中的数据
+        Search search = new Search.Builder("{\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"filter\": {\n" +
+                "        \"term\": {\n" +
+                "          \"sex\": \"男\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"must\": [\n" +
+                "        {\n" +
+                "          \"match\": {\n" +
+                "            \"favo\": \"羽毛球\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"aggs\": {\n" +
+                "    \"groupByClass\": {\n" +
+                "      \"terms\": {\n" +
+                "        \"field\": \"class_id\"\n" +
+                "      },\n" +
+                "      \"aggs\": {\n" +
+                "        \"groupByAge\": {\n" +
+                "          \"max\": {\n" +
+                "            \"field\": \"age\"\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"from\": 0,\n" +
+                "  \"size\": 2\n" +
+                "}")
+                .addType("_doc")
+                .addIndex("student")
+                .build();
+        SearchResult result = jestClient.execute(search);
+
+        //获取命中总数
+        System.out.println("命中条数："+result.getTotal());
+
+        List<SearchResult.Hit<Map, Void>> hits = result.getHits(Map.class);
+        for (SearchResult.Hit<Map, Void> hit : hits) {
+            System.out.println("_index:"+hit.index);
+            System.out.println("_type:"+hit.type);
+            System.out.println("_id:"+hit.id);
+            //获取数据明细
+            Map source = hit.source;
+            for (Object o : source.keySet()) {
+                System.out.println(o+":"+source.get(o));
+            }
+        }
+
+        //获取聚合组数据
+        MetricAggregation aggregations = result.getAggregations();
+        //获取班级聚合组数据
+        TermsAggregation groupByClass = aggregations.getTermsAggregation("groupByClass");
+        List<TermsAggregation.Entry> buckets = groupByClass.getBuckets();
+        for (TermsAggregation.Entry bucket : buckets) {
+            System.out.println("key:"+bucket.getKey());
+            System.out.println("keyAsString:"+bucket.getKeyAsString());
+            System.out.println("doc_count:"+bucket.getCount());
+
+            //获取年龄聚合组数据
+            MaxAggregation groupByAge = bucket.getMaxAggregation("groupByAge");
+            System.out.println("value:"+groupByAge.getMax());
+        }
+
+
+        //关闭连接
+        jestClient.shutdownClient();
+    }
+}
---
+++ gmall-realtime/src/main/scala/com/atguigu/app/AlertApp.scala
@@ -0,0 +1,26 @@
+package com.atguigu.app
+
+object AlertApp {
+  def main(args: Array[String]): Unit = {
+    //1.创建SparkConf
+
+    //2.创建StreamingContext
+
+    //3.消费kafka中用户行为的事件日志
+
+    //4.将读取过来的JSON字符串转为样例类
+
+    //5.开启一个5min的窗口
+
+    //6.对相同mid的数据进行聚合
+
+    //7.根据用户行为进行过滤去重
+
+    //8.生成预警日志
+
+    //9.保存至ES
+
+    //10.开启并阻塞
+  }
+
+}
